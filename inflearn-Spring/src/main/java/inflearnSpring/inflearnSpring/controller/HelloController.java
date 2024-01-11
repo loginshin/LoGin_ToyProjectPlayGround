c0e96d19bb4eb56230ec19cc4b45b845db0c5fe8
loginshin <95666408+loginshin@users.noreply.github.com>
2024-01-12 00:32:00 +0900

    feat)classApiTest

--- inflearn-Spring/src/main/java/inflearnSpring/inflearnSpring/controller/HelloController.java
+++ inflearn-Spring/src/main/java/inflearnSpring/inflearnSpring/controller/HelloController.java
@@ -3,6 +3,8 @@ package inflearnSpring.inflearnSpring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,6 +12,32 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","hello");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName("hello LoGinShin");
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+
+        }
     }
 
 }
