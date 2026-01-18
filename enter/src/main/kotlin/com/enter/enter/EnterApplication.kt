4bb4492421e12a2dbc086fddc4b2982325be92d2
신정환 <loginshin3@gmail.com>
2026-01-18 15:34:47 +0900

    Init Project

---
+++ enter/src/main/kotlin/com/enter/enter/EnterApplication.kt
@@ -0,0 +1,11 @@
+package com.enter.enter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnterApplication
+
+fun main(args: Array<String>) {
+	runApplication<EnterApplication>(*args)
+}
