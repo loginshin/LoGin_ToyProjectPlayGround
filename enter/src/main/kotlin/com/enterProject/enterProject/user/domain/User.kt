388f9c4e72873c3b1354f10f183bf98fe9e90bbd
신정환 <loginshin3@gmail.com>
2026-01-21 23:27:24 +0900

    ✨init) user Entity 작성

---
+++ enter/src/main/kotlin/com/enterProject/enterProject/user/domain/User.kt
@@ -0,0 +1,26 @@
+package com.enterProject.enterProject.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+class User {
+
+    @Id
+    @Column(name = "user_key", length = 128, nullable = false)
+    var userKey: String,
+
+    @Column(name = "user_id", length = 128, unique = true)
+    var userId: String? = null,
+
+    @Column(name = "user_name", length = 128)
+    var userName: String? = null,
+
+    @Column(name = "password", length = 128)
+    var password: String? = null
+
+
+}
