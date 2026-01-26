8c2c81a2001d90c806f40b281c26ff5363e09cf2
LoGinShin <95666408+loginshin@users.noreply.github.com>
2026-01-26 18:41:26 +0900

    SQL) create table users

--- enterProject/DB/users.sql
+++ enterProject/DB/users.sql
@@ -1,13 +1,15 @@
+drop table users;
+
 create table users (
-    user_key VARCHAR2(128) NOT NULL,
-    user_id VARCHAR2(128),
-    user_name VARCHAR2(128),
-    password VARCHAR2(128),
-    email VARCHAR2(128),
-    create_dt TIMESTAMP,
-    update_dt TIMESTAMP
-    CONSTRAINT awf_user_pk PRIMARY KEY (user_key),
-    CONSTRAINT awf_user_uk UNIQUE (user_id)
+   user_key VARCHAR(128) NOT NULL,
+   user_id VARCHAR(128),
+   user_name VARCHAR(128),
+   password VARCHAR(128),
+   email VARCHAR(128),
+   create_dt TIMESTAMP,
+   update_dt TIMESTAMP,
+   CONSTRAINT users_pk PRIMARY KEY (user_key),
+   CONSTRAINT users_uk UNIQUE (user_id)
 );
 
 COMMENT ON TABLE users IS '사용자 정보';
