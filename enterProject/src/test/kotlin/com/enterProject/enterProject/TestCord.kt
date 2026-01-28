92a9a752df729b3764577ff4f50196336442de7a
LoGinShin <95666408+loginshin@users.noreply.github.com>
2026-01-28 19:35:02 +0900

    ✨) modify test code

--- enterProject/src/test/kotlin/com/enterProject/enterProject/TestCord.kt
+++ enterProject/src/test/kotlin/com/enterProject/enterProject/TestCord.kt
@@ -8,13 +8,17 @@ class TestCord {
     @Test
     fun testMain() {
         print("\n\n\n ===================================\n\n\n")
-        print("Hello world!, test start")
+
+        var tmp : String
+        var arr : Array<Int>
+
+        arr = arrayOf(1,2,3,4,5)
+
+        for (i in arr) {
+            print(i * 2)
+        }
 
 
-
-
-
-        print("good bye world!, test End")
         print("\n\n\n===================================\n\n\n ")
     }
 }
